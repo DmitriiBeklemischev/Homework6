c98b114320875d46495801a8c4c6de604f706f18
«Dmitrii <Ambal003@ya.ry>
2022-08-07 20:38:39 +0300

    Сделал первое задание

---
+++ Task#1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int getBigO( int[] arr )
+{
+    int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]>0) res++;
+    
+}
+ return res;
+}
+
+
+Console.WriteLine("Введите количество чисел");
+int col = int.Parse(Console.ReadLine());
+int[] array = new int[col];
+
+for (int i = 0; i < col; i++)
+{
+    Console.WriteLine($"Введите {i+1}е чиселo:");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"Сумма четных чисел = {getBigO( array )}");
